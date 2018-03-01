ef0bd0d7e6b5014f866e4b81f8c9c49ca7ed637e
Gabriel Majeri <gabriel.majeri6@gmail.com>
2018-03-01 19:55:01 +0200

    Store a global reference to the UEFI system table
    This is useful for building simpler abstractions.

--- uefi-services/src/lib.rs
+++ uefi-services/src/lib.rs
@@ -1,3 +1,14 @@
+//! This crate simplifies the writing of higher-level code for UEFI.
+//!
+//! It initializes the memory allocation and logging crates,
+//! allowing code to use Rust's data structures and to log errors.
+//!
+//! It also stores a global reference to the UEFI system table,
+//! in order to reduce the redundant passing of references to it.
+//!
+//! Library code can simply use global UEFI functions
+//! through the reference provided by `system_table`.
+
 #![no_std]
 
 #![feature(compiler_builtins_lib)]
@@ -21,16 +32,42 @@ extern crate log;
 
 use uefi::table::SystemTable;
 
+/// Reference to the system table.
+static mut SYSTEM_TABLE: Option<&'static SystemTable> = None;
+
+/// Obtains a reference to the system table.
+///
+/// This is meant to be used by higher-level libraries,
+/// which want a convenient way to access the system table singleton.
+///
+/// `init` must have been called first by the UEFI app.
+pub fn system_table() -> &'static SystemTable {
+    unsafe {
+        SYSTEM_TABLE.expect("The uefi-services library has not yet been initialized")
+    }
+}
+
 /// Initialize the UEFI utility library.
 ///
 /// This must be called as early as possible,
 /// before trying to use logging or memory allocation capabilities.
 pub fn init(st: &'static SystemTable) {
-    init_logger(st);
-    init_alloc(st);
+    unsafe {
+        // Avoid double initialization.
+        if SYSTEM_TABLE.is_some() {
+            return;
+        }
+
+        SYSTEM_TABLE = Some(st);
+    }
+
+    init_logger();
+    init_alloc();
 }
 
-fn init_logger(st: &'static SystemTable) {
+fn init_logger() {
+    let st = system_table();
+
     static mut LOGGER: Option<uefi_logger::Logger> = None;
 
     let stdout = st.stdout();
@@ -49,7 +86,9 @@ fn init_logger(st: &'static SystemTable) {
     log::set_max_level(log::LevelFilter::Info);
 }
 
-fn init_alloc(st: &'static SystemTable) {
+fn init_alloc() {
+    let st = system_table();
+
     uefi_alloc::init(st.boot);
 }
 

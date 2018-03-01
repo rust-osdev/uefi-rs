9e524b36a5b8980a105a4f00fb7c9ef10689a55f
Gabriel Majeri <gabriel.majeri6@gmail.com>
2018-03-01 19:07:30 +0200

    Improve top-level crate documentation
    Might help contributors to the crate to understand the crate's structure.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,27 @@
 //! Rusty wrapper for the Unified Extensible Firmware Interface.
+//!
+//! # Crate organisation
+//!
+//! The top-level module contains some of the most used types,
+//! such as the result and error types, or other common data structures
+//! such as GUIDs and handles.
+//!
+//! ## Tables and protocols
+//!
+//! The `table` module contains definitions of the UEFI tables,
+//! which are structures containing some basic functions and references to other tables.
+//! Most importantly, the boot services table also provides a way to obtain **protocol** handles.
+//!
+//! The `proto` module contains the standard UEFI protocols, which are normally provided
+//! by the various UEFI drivers and firmware layers.
+//!
+//! ## Adapting to local conditions
+//!
+//! Unlike system tables, which are present on *all* UEFI implementations,
+//! protocols *may* or *may not* be present on a certain system.
+//!
+//! For example, a PC with no network card might not contain a network driver,
+//! therefore all the network protocols will be unavailable.
 
 #![feature(try_trait)]
 #![feature(optin_builtin_traits)]

4639a8546cafa9916ee9fad4f4fd8ce63da39f40
Hadrien G <knights_of_ni@gmx.com>
2018-09-23 17:40:25 +0200

    Run cargo fmt

--- uefi-test-runner/src/main.rs
+++ uefi-test-runner/src/main.rs
@@ -57,26 +57,36 @@ fn check_revision(rev: uefi::table::Revision) {
 /// TODO: Turn it into something which waits a bit for user inspection elsewhere
 fn check_screenshot(bt: &BootServices, name: &str) {
     // Access the serial port (in a QEMU environment, it should always be there)
-    let mut serial = bt.find_protocol::<Serial>()
-                       .expect("Could not find serial port");
+    let mut serial = bt
+        .find_protocol::<Serial>()
+        .expect("Could not find serial port");
     let serial = unsafe { serial.as_mut() };
 
     // Set a large-ish timeout to avoid problems
     let mut io_mode = serial.io_mode().clone();
     io_mode.timeout = 100_000;
-    serial.set_attributes(&io_mode)
-          .expect("Failed to configure serial port");
+    serial
+        .set_attributes(&io_mode)
+        .expect("Failed to configure serial port");
 
     // Send a screenshot request to QEMU
     // TODO: Do not hardcode the screenshot name
     // TODO: Implement write!() for serial ports
     let screenshot_request = b"SCREENSHOT: gop_test\n";
-    let write_size = serial.write(screenshot_request).expect("Failed to write screenshot command");
-    assert_eq!(write_size, screenshot_request.len(), "Screenshot request timed out");
+    let write_size = serial
+        .write(screenshot_request)
+        .expect("Failed to write screenshot command");
+    assert_eq!(
+        write_size,
+        screenshot_request.len(),
+        "Screenshot request timed out"
+    );
 
     // Wait for QEMU's acknowledgement before moving forward
     let mut reply = [0; 3];
-    let read_size = serial.read(&mut reply[..]).expect("Failed to read host reply");
+    let read_size = serial
+        .read(&mut reply[..])
+        .expect("Failed to read host reply");
     assert_eq!(read_size, 3, "Screenshot request timed out");
     assert_eq!(&reply[..], b"OK\n", "Unexpected screenshot request reply");
 }

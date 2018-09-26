68894466dd0559addfd1f0c6ebd7a5e89557e1e7
Hadrien Grasland <grasland@lal.in2p3.fr>
2018-09-26 12:03:45 +0200

    Update OVMF image

--- uefi-test-runner/ci/ovmf.sh
+++ uefi-test-runner/ci/ovmf.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-wget https://www.kraxel.org/repos/jenkins/edk2/edk2.git-ovmf-x64-0-20180807.263.g3cb0a311cb.noarch.rpm -O ovmf.rpm
+wget https://www.kraxel.org/repos/jenkins/edk2/edk2.git-ovmf-x64-0-20180807.267.g447b08b3d2.noarch.rpm -O ovmf.rpm
 
 # Need to use bsdtar to extract RPMs
 bsdtar xvf ovmf.rpm

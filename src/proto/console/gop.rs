cfbf00a11ef76802d518d4f34b3a37c19dd078f5
Hadrien G <knights_of_ni@gmx.com>
2018-09-27 20:43:43 +0200

    Marking the GOP's PixelFormat enum as safe

--- src/proto/console/gop.rs
+++ src/proto/console/gop.rs
@@ -295,7 +295,7 @@ struct ModeData {
 #[repr(u32)]
 pub enum PixelFormat {
     /// Each pixel is 32-bit long, with 24-bit RGB, and the last byte is reserved.
-    RGB,
+    RGB = 0,
     /// Each pixel is 32-bit long, with 24-bit BGR, and the last byte is reserved.
     BGR,
     /// Custom pixel format, check the associated bitmask.
@@ -305,6 +305,11 @@ pub enum PixelFormat {
     /// This means you will have to use the `blt` function which will
     /// convert the graphics data to the device's internal pixel format.
     BltOnly,
+
+    // NOTE: UEFI also defines a PixelFormatMax variant, and states that all
+    //       valid enum values are guaranteed to be smaller. Since that is the
+    //       case, adding a new enum variant would be a breaking change, so it
+    //       is safe to model this C enum as a Rust enum.
 }
 
 /// Bitmask used to indicate which bits of a pixel represent a given color.

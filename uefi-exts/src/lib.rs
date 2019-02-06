02616ac979f4fa1fb2aeaf5f2efd51b71caa1fcd
Joe Richey <joerichey@google.com>
2019-02-05 17:08:35 -0800

    Properly handle allocation failure
    The change in #89 did not properly handle allocation failures. On
    failure, alloc() returns a null pointer, and this is not checked.
    
    This change moves buffer allocation to the new Alloc API, correctly
    handles allocation failure, and avoids potential unsafety with
    zero-sized alloc requests.
    
    I also removed the dependancy on uefi-services from uefi-alloc. The
    dependancy isn't needed, and now users can use uefi-exts with their
    own allocators.

--- uefi-exts/src/lib.rs
+++ uefi-exts/src/lib.rs
@@ -4,7 +4,7 @@
 //! which add utility functions to various UEFI objects.
 
 #![no_std]
-#![feature(alloc, alloc_layout_extra)]
+#![feature(alloc, alloc_layout_extra, allocator_api)]
 
 extern crate alloc;
 
@@ -13,3 +13,24 @@ mod file;
 
 pub use self::boot::BootServicesExt;
 pub use self::file::FileExt;
+
+use alloc::{
+    alloc::{handle_alloc_error, Alloc, Global, Layout},
+    boxed::Box,
+};
+use core::slice;
+
+/// Creates a boxed byte buffer using the standard allocator
+/// # Panics
+/// Calls handle_alloc_error if the layout has a size of zero or allocation fails.
+pub fn allocate_buffer(layout: Layout) -> Box<[u8]> {
+    if layout.size() == 0 {
+        handle_alloc_error(layout);
+    }
+    unsafe {
+        match Global.alloc(layout) {
+            Ok(mem) => Box::from_raw(slice::from_raw_parts_mut(mem.as_ptr(), layout.size())),
+            Err(_) => handle_alloc_error(layout),
+        }
+    }
+}

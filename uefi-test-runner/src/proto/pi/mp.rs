414f98f859a1a15a8c84fbe244279a8121cab0ca
Gabriel Majeri <gabriel.majeri6@gmail.com>
2019-08-08 22:39:14 +0300

    Run cargo-fmt on all crates

--- uefi-test-runner/src/proto/pi/mp.rs
+++ uefi-test-runner/src/proto/pi/mp.rs
@@ -1,6 +1,6 @@
-use core::time::Duration;
 use core::ffi::c_void;
 use core::sync::atomic::{AtomicUsize, Ordering};
+use core::time::Duration;
 use uefi::proto::pi::mp::MPServices;
 use uefi::table::boot::BootServices;
 use uefi::Status;
@@ -8,8 +8,8 @@ use uefi::Status;
 pub fn test(bt: &BootServices) {
     info!("Running UEFI multi-processor services protocol test");
     if let Ok(mp_support) = bt.locate_protocol::<MPServices>() {
-        let mp_support =
-            mp_support.expect("Warnings encountered while opening multi-processor services protocol");
+        let mp_support = mp_support
+            .expect("Warnings encountered while opening multi-processor services protocol");
         let mp_support = unsafe { &mut *mp_support.get() };
 
         test_get_number_of_processors(mp_support);
@@ -75,12 +75,19 @@ fn test_startup_all_aps(mps: &MPServices, bt: &BootServices) {
     // Ensure that APs start up
     let counter = AtomicUsize::new(0);
     let counter_ptr: *mut c_void = &counter as *const _ as *mut _;
-    mps.startup_all_aps(false, proc_increment_atomic, counter_ptr, None).unwrap().unwrap();
+    mps.startup_all_aps(false, proc_increment_atomic, counter_ptr, None)
+        .unwrap()
+        .unwrap();
     assert_eq!(counter.load(Ordering::Relaxed), 2);
 
     // Make sure that timeout works
     let bt_ptr: *mut c_void = bt as *const _ as *mut _;
-    let ret = mps.startup_all_aps(false, proc_wait_100ms, bt_ptr, Some(Duration::from_millis(50)));
+    let ret = mps.startup_all_aps(
+        false,
+        proc_wait_100ms,
+        bt_ptr,
+        Some(Duration::from_millis(50)),
+    );
     assert_eq!(ret.map_err(|err| err.status()), Err(Status::TIMEOUT));
 }
 
@@ -88,8 +95,12 @@ fn test_startup_this_ap(mps: &MPServices, bt: &BootServices) {
     // Ensure that each AP starts up
     let counter = AtomicUsize::new(0);
     let counter_ptr: *mut c_void = &counter as *const _ as *mut _;
-    mps.startup_this_ap(1, proc_increment_atomic, counter_ptr, None).unwrap().unwrap();
-    mps.startup_this_ap(2, proc_increment_atomic, counter_ptr, None).unwrap().unwrap();
+    mps.startup_this_ap(1, proc_increment_atomic, counter_ptr, None)
+        .unwrap()
+        .unwrap();
+    mps.startup_this_ap(2, proc_increment_atomic, counter_ptr, None)
+        .unwrap()
+        .unwrap();
     assert_eq!(counter.load(Ordering::Relaxed), 2);
 
     // Make sure that timeout works for each AP
@@ -116,7 +127,9 @@ fn test_enable_disable_ap(mps: &MPServices) {
     assert_eq!(proc_count.total, proc_count.enabled);
 
     // Mark second CPU as unhealthy and check it's status
-    mps.enable_disable_ap(1, true, Some(false)).unwrap().unwrap();
+    mps.enable_disable_ap(1, true, Some(false))
+        .unwrap()
+        .unwrap();
     let cpu1 = mps.get_processor_info(1).unwrap().unwrap();
     assert_eq!(cpu1.is_healthy(), false);
 

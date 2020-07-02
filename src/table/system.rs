0aa939fe6f3b425ee21e6b0a1c65d667a3a41fd4
Steve Fan <19037626d@connect.polyu.hk>
2020-07-02 23:19:29 +0800

    Revert "allow boot and runtime service table to be cloned"
    This reverts commit 93d337f5

--- src/table/system.rs
+++ src/table/system.rs
@@ -12,13 +12,10 @@ use super::{cfg, Header, Revision};
 pub trait SystemTableView {}
 
 /// Marker struct associated with the boot view of the UEFI System Table
-#[derive(Clone)]
 pub struct Boot;
-
 impl SystemTableView for Boot {}
 
 /// Marker struct associated with the run-time view of the UEFI System Table
-#[derive(Clone)]
 pub struct Runtime;
 impl SystemTableView for Runtime {}
 
@@ -44,7 +41,6 @@ impl SystemTableView for Runtime {}
 /// UEFI boot services in the eye of the Rust borrow checker) and a runtime view
 /// will be provided to replace it.
 #[repr(transparent)]
-#[derive(Clone)]
 pub struct SystemTable<View: SystemTableView> {
     table: &'static SystemTableImpl,
     _marker: PhantomData<View>,

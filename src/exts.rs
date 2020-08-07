b2c75833c33102dfc38c87c07e8e4e9e054c2b99
Gabriel Majeri <gabriel.majeri6@gmail.com>
2020-08-07 07:55:18 +0300

    Update to new `alloc` API

--- src/exts.rs
+++ src/exts.rs
@@ -1,12 +1,12 @@
 //! Utility functions for the most common UEFI patterns.
 
 use alloc_api::{
-    alloc::{handle_alloc_error, AllocInit, AllocRef, Global, MemoryBlock},
+    alloc::{handle_alloc_error, AllocRef, Global},
     boxed::Box,
 };
-use core::{alloc::Layout, slice};
+use core::alloc::Layout;
 
-/// Creates a boxed byte buffer using the standard allocator
+/// Creates a boxed byte buffer using the standard allocator.
 ///
 /// # Panics
 ///
@@ -16,10 +16,10 @@ pub fn allocate_buffer(layout: Layout) -> Box<[u8]> {
         handle_alloc_error(layout);
     }
     unsafe {
-        let MemoryBlock { ptr, size } = match Global.alloc(layout, AllocInit::Uninitialized) {
-            Ok(block) => block,
+        let mut slice = match Global.alloc(layout) {
+            Ok(slice) => slice,
             Err(_) => handle_alloc_error(layout),
         };
-        Box::from_raw(slice::from_raw_parts_mut(ptr.as_ptr(), size))
+        Box::from_raw(slice.as_mut())
     }
 }

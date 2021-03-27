9d138cd142a95a6ae95a3944645399e4484ce803
Gabriel Majeri <gabriel.majeri6@gmail.com>
2021-03-27 11:28:19 +0200

    Revert commenting of exit boot services call

--- uefi-test-runner/src/main.rs
+++ uefi-test-runner/src/main.rs
@@ -121,13 +121,13 @@ fn shutdown(image: uefi::Handle, st: SystemTable<Boot>) -> ! {
         info!("Testing complete, shutting down...");
     }
 
-    // // Exit boot services as a proof that it works :)
-    // let max_mmap_size =
-    //     st.boot_services().memory_map_size() + 8 * mem::size_of::<MemoryDescriptor>();
-    // let mut mmap_storage = vec![0; max_mmap_size].into_boxed_slice();
-    // let (st, _iter) = st
-    //     .exit_boot_services(image, &mut mmap_storage[..])
-    //     .expect_success("Failed to exit boot services");
+    // Exit boot services as a proof that it works :)
+    let max_mmap_size =
+        st.boot_services().memory_map_size() + 8 * mem::size_of::<MemoryDescriptor>();
+    let mut mmap_storage = vec![0; max_mmap_size].into_boxed_slice();
+    let (st, _iter) = st
+        .exit_boot_services(image, &mut mmap_storage[..])
+        .expect_success("Failed to exit boot services");
 
     // Shut down the system
     let rt = unsafe { st.runtime_services() };

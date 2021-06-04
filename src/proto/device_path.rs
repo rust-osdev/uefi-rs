db52005541045e34e5f10a31ce0a99b6b48f3acc
Nicholas Bishop <nicholasbishop@google.com>
2021-06-04 08:19:41 -0400

    Use newtype_enum for DevicePath enums (#230)

--- src/proto/device_path.rs
+++ src/proto/device_path.rs
@@ -19,53 +19,51 @@ pub struct DevicePath {
     pub length: [u8; 2],
 }
 
+newtype_enum! {
 /// Type identifier for a DevicePath
-#[repr(u8)]
-#[derive(Debug, PartialEq)]
-pub enum DeviceType {
+pub enum DeviceType: u8 => {
     /// Hardware Device Path.
     ///
     /// This Device Path defines how a device is attached to the resource domain of a system, where resource domain is
     /// simply the shared memory, memory mapped I/ O, and I/O space of the system.
-    Hardware = 0x01,
+    HARDWARE = 0x01,
     /// ACPI Device Path.
     ///
     /// This Device Path is used to describe devices whose enumeration is not described in an industry-standard fashion.
     /// These devices must be described using ACPI AML in the ACPI namespace; this Device Path is a linkage to the ACPI
     /// namespace.
-    Acpi = 0x02,
+    ACPI = 0x02,
     /// Messaging Device Path.
     ///
     /// This Device Path is used to describe the connection of devices outside the resource domain of the system. This
     /// Device Path can describe physical messaging information such as a SCSI ID, or abstract information such as
     /// networking protocol IP addresses.
-    Messaging = 0x03,
+    MESSAGING = 0x03,
     /// Media Device Path.
     ///
     /// This Device Path is used to describe the portion of a medium that is being abstracted by a boot service.
     /// For example, a Media Device Path could define which partition on a hard drive was being used.
-    Media = 0x04,
+    MEDIA = 0x04,
     /// BIOS Boot Specification Device Path.
     ///
     /// This Device Path is used to point to boot legacy operating systems; it is based on the BIOS Boot Specification
     /// Version 1.01.
-    BiosBootSpec = 0x05,
+    BIOS_BOOT_SPEC = 0x05,
     /// End of Hardware Device Path.
     ///
     /// Depending on the Sub-Type, this Device Path node is used to indicate the end of the Device Path instance or
     /// Device Path structure.
-    End = 0x7F,
-}
+    END = 0x7F,
+}}
 
+newtype_enum! {
 /// Sub-type identifier for a DevicePath
-#[repr(u8)]
-#[derive(Debug)]
-pub enum DeviceSubType {
+pub enum DeviceSubType: u8 => {
     /// End This Instance of a Device Path and start a new Device Path
-    EndInstance = 0x01,
+    END_INSTANCE = 0x01,
     /// End Entire Device Path
-    EndEntire = 0xFF,
-}
+    END_ENTIRE = 0xFF,
+}}
 
 /// ACPI Device Path
 #[repr(C)]

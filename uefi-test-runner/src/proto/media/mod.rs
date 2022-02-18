103e12a8eda64818646c64a6d1333e9e089195b3
Nicholas Bishop <nbishop@nbishop.net>
2022-02-18 17:32:07 -0500

    Add a test for opening and reading a file

--- uefi-test-runner/src/proto/media/mod.rs
+++ uefi-test-runner/src/proto/media/mod.rs
@@ -1,7 +1,41 @@
+use core::convert::TryFrom;
 use uefi::prelude::*;
+use uefi::proto::media::file::{Directory, File, FileAttribute, FileMode, FileType};
 use uefi::proto::media::fs::SimpleFileSystem;
 use uefi::proto::media::partition::PartitionInfo;
 use uefi::table::boot::{OpenProtocolAttributes, OpenProtocolParams};
+use uefi::CString16;
+
+/// Open and read a test file in the boot directory.
+pub fn test_open_and_read(directory: &mut Directory) {
+    let test_input_path = CString16::try_from("EFI\\BOOT\\test_input.txt").unwrap();
+    match directory.open(&test_input_path, FileMode::Read, FileAttribute::empty()) {
+        Ok(file) => {
+            let file = file.unwrap().into_type().unwrap_success();
+            if let FileType::Regular(mut file) = file {
+                let mut buffer = vec![0; 128];
+                let size = file
+                    .read(&mut buffer)
+                    .expect_success(&format!("failed to read {}", test_input_path));
+                let buffer = &buffer[..size];
+                info!("Successfully read {}", test_input_path);
+                assert_eq!(buffer, b"test input data");
+            } else {
+                panic!("{} is not a regular file", test_input_path);
+            }
+        }
+        Err(err) => {
+            let msg = format!("Failed to open {}: {:?}", test_input_path, err);
+            // The file might reasonably not be present when running on real
+            // hardware, so only panic on failure under qemu.
+            if cfg!(feature = "qemu") {
+                panic!("{}", msg);
+            } else {
+                warn!("{}", msg);
+            }
+        }
+    }
+}
 
 pub fn test(image: Handle, bt: &BootServices) {
     info!("Testing Media Access protocols");
@@ -31,6 +65,8 @@ pub fn test(image: Handle, bt: &BootServices) {
             info!("Root directory entry: {:?}", file_info);
         }
         directory.reset_entry_readout().unwrap().unwrap();
+
+        test_open_and_read(&mut directory);
     } else {
         warn!("`SimpleFileSystem` protocol is not available");
     }

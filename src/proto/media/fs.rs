f56888a8a43468a24ecff63577bca81d17509756
Ryan Cohen <rcohenprogramming@gmail.com>
2022-04-30 13:30:53 -0400

    Add documentation links for `SimpleFileSystem`.
    Adds documentation for rust-osdev#416

--- src/proto/media/fs.rs
+++ src/proto/media/fs.rs
@@ -9,6 +9,16 @@ use core::ptr;
 ///
 /// This interface is implemented by some storage devices
 /// to allow file access to the contained file systems.
+///
+/// # Accessing `SimpleFileSystem` protocol
+///
+/// Use [`BootServices::get_image_file_system`] to retrieve the `SimpleFileSystem`
+/// protocol associated with a given image handle.
+///
+/// See the [`BootServices`] documentation for more details of how to open a protocol.
+///
+/// [`BootServices::get_image_file_system`]: crate::table::boot::BootServices::get_image_file_system
+/// [`BootServices`]: crate::table::boot::BootServices#accessing-protocols
 #[repr(C)]
 #[unsafe_guid("964e5b22-6459-11d2-8e39-00a0c969723b")]
 #[derive(Protocol)]

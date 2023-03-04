50d05cc8ce8b02279c85be6f7885f35df542961c
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-03-04 12:43:31 +0800

    update crate feature flags

--- uefi/src/lib.rs
+++ uefi/src/lib.rs
@@ -58,7 +58,7 @@
 //! [`uefi-services`]: https://crates.io/crates/uefi-services
 //! [unstable features]: https://doc.rust-lang.org/unstable-book/
 
-#![feature(abi_efiapi)]
+#![feature(extended_varargs_abi_support)]
 #![cfg_attr(feature = "unstable", feature(error_in_core))]
 #![cfg_attr(all(feature = "unstable", feature = "alloc"), feature(allocator_api))]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]

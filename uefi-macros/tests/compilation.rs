c72e024a7792ffe9377916f1620763675fca1b2f
Nicholas Bishop <nbishop@nbishop.net>
2023-05-09 18:51:10 -0400

    uefi-macros: Add pass tests for the entry macro
    These new tests cover various valid uses of the `entry` macro.

--- uefi-macros/tests/compilation.rs
+++ uefi-macros/tests/compilation.rs
@@ -2,4 +2,5 @@
 fn ui() {
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/ui/fail/*.rs");
+    t.pass("tests/ui/pass/*.rs");
 }
---
+++ uefi-macros/tests/ui/pass/entry_unsafe.rs
@@ -0,0 +1,10 @@
+use uefi::table::{Boot, SystemTable};
+use uefi::{entry, Handle, Status};
+
+#[entry]
+unsafe fn efi_main(image: Handle, st: SystemTable<Boot>) -> Status {
+    Status::SUCCESS
+}
+
+// trybuild requires a `main` function.
+fn main() {}

f34bc480ce90b77623543648ca28dc36f25ae4e0
Philipp Schuster <phip1611@gmail.com>
2023-05-25 20:17:40 +0200

    nix: switch to niv instead of non-standard dependency management

--- shell.nix
+++ shell.nix
@@ -1,9 +1,17 @@
-let
-  pkgsSrc = import ./nix/nixpkgs.nix;
-  pkgs = import pkgsSrc {};
-in
+# Sets up a basic shell environment with all relevant tooling to run
+# "cargo xtask run|test|clippy". It uses rustup rather than a pinned rust
+# toolchain.
+
+{ sources ? import ./nix/sources.nix { }
+, pkgs ? import sources.nixpkgs { }
+}:
+
 pkgs.mkShell rec {
   nativeBuildInputs = with pkgs; [
+    # nix related stuff (such as dependency management)
+    niv
+    nixpkgs-fmt
+
     rustup
     qemu
   ];
@@ -15,4 +23,9 @@ pkgs.mkShell rec {
   # OVMF_CODE="${pkgs.OVMF.firmware}";
   # OVMF_VARS="${pkgs.OVMF.variables}";
   # OVMF_SHELL="${pkgs.edk2-uefi-shell}";
+
+  # To invoke "nix-shell" in the CI-runner, we need a global Nix channel.
+  # For better reproducibility inside the Nix shell, we override this channel
+  # with the pinned nixpkgs version.
+  NIX_PATH = "nixpkgs=${sources.nixpkgs}";
 }

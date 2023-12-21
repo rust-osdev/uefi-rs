d60ce4ab7641afd21e729708d5e954513648911e
Nicholas Bishop <nbishop@nbishop.net>
2023-12-20 23:11:47 -0500

    uefi-raw: Derive Default for address types

--- uefi-raw/src/lib.rs
+++ uefi-raw/src/lib.rs
@@ -59,16 +59,16 @@ pub type PhysicalAddress = u64;
 pub type VirtualAddress = u64;
 
 /// An IPv4 internet protocol address.
-#[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
+#[derive(Clone, Copy, Debug, Default, Eq, PartialEq, Ord, PartialOrd, Hash)]
 #[repr(transparent)]
 pub struct Ipv4Address(pub [u8; 4]);
 
 /// An IPv6 internet protocol address.
-#[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
+#[derive(Clone, Copy, Debug, Default, Eq, PartialEq, Ord, PartialOrd, Hash)]
 #[repr(transparent)]
 pub struct Ipv6Address(pub [u8; 16]);
 
 /// A Media Access Control (MAC) address.
-#[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
+#[derive(Clone, Copy, Debug, Default, Eq, PartialEq, Ord, PartialOrd, Hash)]
 #[repr(transparent)]
 pub struct MacAddress(pub [u8; 32]);

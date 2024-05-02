6cff6c5aad5309c93914944e3af46f799ff17bb2
Nicholas Bishop <nicholasbishop@google.com>
2024-05-02 15:09:53 -0400

    uefi: Sort the code in table/mod.rs

--- uefi/src/table/mod.rs
+++ uefi/src/table/mod.rs
@@ -1,21 +1,19 @@
 //! Standard UEFI tables.
 
+pub mod boot;
+pub mod cfg;
+pub mod runtime;
+
+mod header;
+mod system;
+
+pub use header::Header;
+pub use system::{Boot, Runtime, SystemTable};
+pub use uefi_raw::table::Revision;
+
 /// Common trait implemented by all standard UEFI tables.
 pub trait Table {
     /// A unique number assigned by the UEFI specification
     /// to the standard tables.
     const SIGNATURE: u64;
 }
-
-mod header;
-pub use header::Header;
-
-mod system;
-pub use system::{Boot, Runtime, SystemTable};
-
-pub mod boot;
-pub mod runtime;
-
-pub mod cfg;
-
-pub use uefi_raw::table::Revision;

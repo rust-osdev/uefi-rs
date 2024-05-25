e47e36c1fca1597e84095d1332c203678f192043
Philipp Schuster <phip1611@gmail.com>
2024-05-26 00:19:18 +0200

    uefi-test-runner: test that BootService::memory_map memory is freed
    This is now a benefit compared to the old API. This wasn't possible.

--- uefi-test-runner/src/boot/memory.rs
+++ uefi-test-runner/src/boot/memory.rs
@@ -63,36 +63,41 @@ fn alloc_alignment() {
 fn memory_map(bt: &BootServices) {
     info!("Testing memory map functions");
 
-    let mut memory_map = bt
-        .memory_map(MemoryType::LOADER_DATA)
-        .expect("Failed to retrieve UEFI memory map");
+    // Ensure that the memory map is freed after each iteration (on drop).
+    // Otherwise, we will have an OOM.
+    for _ in 0..200000 {
+        let mut memory_map = bt
+            .memory_map(MemoryType::LOADER_DATA)
+            .expect("Failed to retrieve UEFI memory map");
 
-    memory_map.sort();
+        memory_map.sort();
 
-    // Collect the descriptors into a vector
-    let descriptors = memory_map.entries().copied().collect::<Vec<_>>();
+        // Collect the descriptors into a vector
+        let descriptors = memory_map.entries().copied().collect::<Vec<_>>();
 
-    // Ensured we have at least one entry.
-    // Real memory maps usually have dozens of entries.
-    assert!(!descriptors.is_empty(), "Memory map is empty");
+        // Ensured we have at least one entry.
+        // Real memory maps usually have dozens of entries.
+        assert!(!descriptors.is_empty(), "Memory map is empty");
 
-    let mut curr_value = descriptors[0];
+        let mut curr_value = descriptors[0];
 
-    for value in descriptors.iter().skip(1) {
-        if value.phys_start <= curr_value.phys_start {
-            panic!("memory map sorting failed");
+        for value in descriptors.iter().skip(1) {
+            if value.phys_start <= curr_value.phys_start {
+                panic!("memory map sorting failed");
+            }
+            curr_value = *value;
         }
-        curr_value = *value;
-    }
 
-    // This is pretty much a sanity test to ensure returned memory isn't filled with random values.
-    let first_desc = descriptors[0];
+        // This is pretty much a basic sanity test to ensure returned memory
+        // isn't filled with random values.
+        let first_desc = descriptors[0];
 
-    #[cfg(target_arch = "x86_64")]
-    {
-        let phys_start = first_desc.phys_start;
-        assert_eq!(phys_start, 0, "Memory does not start at address 0");
+        #[cfg(target_arch = "x86_64")]
+        {
+            let phys_start = first_desc.phys_start;
+            assert_eq!(phys_start, 0, "Memory does not start at address 0");
+        }
+        let page_count = first_desc.page_count;
+        assert!(page_count != 0, "Memory map entry has size zero");
     }
-    let page_count = first_desc.page_count;
-    assert!(page_count != 0, "Memory map entry has zero size");
 }

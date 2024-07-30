c6f4859bfd870fb0575b2aba30a65d32f5fdfff5
Philipp Schuster <phip1611@gmail.com>
2024-07-30 10:59:19 +0200

    doc: streamline sections Contributing, MSRV, and License
    Copy MSRV, License, and Contributing sections to lib.rs and streamline it
    with the README equivalents. We should also include these information in
    lib.rs, as it is the main entry point into everything relevant about the
    library. At least, it should from now on.
    
    The big benefit is that the documentation on `docs.rs` then covers
    everything relevant.

--- uefi/src/lib.rs
+++ uefi/src/lib.rs
@@ -103,6 +103,30 @@
 //! only unfold their potential when you invoke `uefi::helpers::init` as soon
 //! as possible in your application.
 //!
+//! # Discuss and Contribute
+//!
+//! For general discussions, feel free to join us in our [Zulip] and ask
+//! your questions there.
+//!
+//! Further, you can submit bugs and also ask questions in our [issue tracker].
+//! Contributions in form of a PR are also highly welcome. Check our
+//! [contributing guide][contributing] for details.
+//!
+//! # MSRV
+//! <!-- Keep in Sync with README! -->
+//!
+//! The minimum supported Rust version is currently 1.70.
+//! Our policy is to support at least the past two stable releases.
+//!
+//! # License
+//! <!-- Keep in Sync with README! -->
+//!
+//! The code in this repository is licensed under the Mozilla Public License 2.
+//! This license allows you to use the crate in proprietary programs, but any
+//! modifications to the files must be open-sourced.
+//!
+//! The full text of the license is available in the [license file][LICENSE].
+//!
 //! # Trivia and Background
 //!
 //! [UEFI] started as the successor firmware to the BIOS in x86 space and
@@ -117,8 +141,10 @@
 //! Even joke projects such as an [IRC client][uefirc] leveraging only existing
 //! UEFI boot services are possible! 😉
 //!
+//! [LICENSE]: https://github.com/rust-osdev/uefi-rs/blob/main/uefi/LICENSE
 //! [Rust UEFI Book]: https://rust-osdev.github.io/uefi-rs/HEAD/
 //! [UEFI]: https://uefi.org/
+//! [Zulip]: https://rust-osdev.zulipchat.com
 //! [`BootServices`]: table::boot::BootServices
 //! [`GlobalAlloc`]: alloc::alloc::GlobalAlloc
 //! [`SystemTable`]: table::SystemTable

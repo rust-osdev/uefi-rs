e0fde842a433d7c71dec8433a10cf0c3dd2c253c
Philipp Schuster <phip1611@gmail.com>
2024-07-30 11:15:13 +0200

    doc: reorder links alphabetically in lib.rs

--- uefi/src/lib.rs
+++ uefi/src/lib.rs
@@ -191,12 +191,11 @@
 //! [`unsafe_protocol`]: proto::unsafe_protocol
 //! [contributing]: https://github.com/rust-osdev/uefi-rs/blob/main/CONTRIBUTING.md
 //! [issue tracker]: https://github.com/rust-osdev/uefi-rs/issues
+//! [rustc-uefi-std]: https://doc.rust-lang.org/nightly/rustc/platform-support/unknown-uefi.html
 //! [spec]: https://uefi.org/specifications
 //! [template]: https://github.com/rust-osdev/uefi-rs/tree/main/template
-//! [unstable features]: https://doc.rust-lang.org/unstable-book/
-//! [uefirc]: https://github.com/codyd51/uefirc
-//! [rustc-uefi-std]: https://doc.rust-lang.org/nightly/rustc/platform-support/unknown-uefi.html
 //! [uefi-std-tr-issue]: https://github.com/rust-lang/rust/issues/100499
+//! [uefirc]: https://github.com/codyd51/uefirc
 
 #![cfg_attr(all(feature = "unstable", feature = "alloc"), feature(allocator_api))]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]

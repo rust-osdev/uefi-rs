5828189e2461d47f467fb634e68a7fb07aa78823
Nicholas Bishop <nicholasbishop@google.com>
2024-08-26 10:20:14 -0400

    uefi: Deprecate BootServices

--- uefi/src/prelude.rs
+++ uefi/src/prelude.rs
@@ -5,6 +5,7 @@
 pub use crate::{cstr16, cstr8, entry, Handle, ResultExt, Status, StatusExt};
 
 // Import the basic table types.
+#[allow(deprecated)]
 pub use crate::table::boot::BootServices;
 #[allow(deprecated)]
 pub use crate::table::runtime::RuntimeServices;

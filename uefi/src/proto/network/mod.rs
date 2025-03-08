2f354ff060d2c880a958cc8e8cd77966e8e5a0fc
Astrid Yu <astrid@astrid.tech>
2025-03-08 15:09:38 -0800

    impl conversions for IpAddress

--- uefi/src/proto/network/mod.rs
+++ uefi/src/proto/network/mod.rs
@@ -34,3 +34,40 @@ impl IpAddress {
         Self(ip_addr)
     }
 }
+
+impl From<core::net::Ipv4Addr> for IpAddress {
+    fn from(t: core::net::Ipv4Addr) -> Self {
+        Self::new_v4(t.octets())
+    }
+}
+
+impl From<IpAddress> for core::net::Ipv4Addr {
+    fn from(IpAddress(o): IpAddress) -> Self {
+        Self::from([o[0], o[1], o[2], o[3]])
+    }
+}
+
+impl From<core::net::Ipv6Addr> for IpAddress {
+    fn from(t: core::net::Ipv6Addr) -> Self {
+        Self::new_v6(t.octets())
+    }
+}
+
+impl From<IpAddress> for core::net::Ipv6Addr {
+    fn from(value: IpAddress) -> Self {
+        Self::from(value.0)
+    }
+}
+
+impl From<core::net::IpAddr> for IpAddress {
+    fn from(t: core::net::IpAddr) -> Self {
+        match t {
+            core::net::IpAddr::V4(a) => a.into(),
+            core::net::IpAddr::V6(a) => a.into(),
+        }
+    }
+}
+
+// NOTE: We cannot impl From<IpAddress> for core::net::IpAddr
+// because IpAddress is a raw union, with nothing indicating
+// whether it should be considered v4 or v6.

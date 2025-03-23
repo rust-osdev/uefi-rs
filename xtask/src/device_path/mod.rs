3cbf2094dd7e4703b285a973c7fbefa31f0c7364
Philipp Schuster <phip1611@gmail.com>
2025-03-23 15:53:53 +0100

    rustfmt: fixes for device_path_gen for 2024 edition

--- xtask/src/device_path/mod.rs
+++ xtask/src/device_path/mod.rs
@@ -73,7 +73,7 @@ fn gen_uefi_code_as_string(groups: &[NodeGroup]) -> Result<String> {
     let code = quote!(
         use bitflags::bitflags;
         use crate::data_types::UnalignedSlice;
-        use crate::{guid, Guid};
+        use crate::{Guid, guid};
         use crate::polyfill::maybe_uninit_slice_as_mut_ptr;
         use crate::proto::device_path::{
             self, DevicePathHeader, DevicePathNode, DeviceSubType, DeviceType,

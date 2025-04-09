cb77172ecbacb13a7ff7547ef1e3351df1ebbad3
Gerd Hoffmann <kraxel@redhat.com>
2025-04-09 12:47:46 +0200

    ip4cfg: add documentation comments
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- uefi/src/proto/network/ip4config2.rs
+++ uefi/src/proto/network/ip4config2.rs
@@ -1,5 +1,7 @@
 #![cfg(target_os = "uefi")]
 
+//! IP4 Config2 Protocol.
+
 extern crate alloc;
 use alloc::vec;
 use alloc::vec::Vec;
@@ -14,11 +16,15 @@ use uefi_raw::protocol::network::ip4_config2::{
 };
 use uefi_raw::Ipv4Address;
 
+/// IP4 Config2 [`Protocol`]. Configure IPv4 networking.
+///
+/// [`Protocol`]: uefi::proto::Protocol
 #[derive(Debug)]
 #[unsafe_protocol(Ip4Config2Protocol::GUID)]
 pub struct Ip4Config2(pub Ip4Config2Protocol);
 
 impl Ip4Config2 {
+    /// Open IP4 Config2 protocol for the given NIC handle.
     pub fn new(nic_handle: Handle) -> uefi::Result<ScopedProtocol<Ip4Config2>> {
         let protocol;
         unsafe {
@@ -34,6 +40,7 @@ impl Ip4Config2 {
         Ok(protocol)
     }
 
+    /// Set configuration data.  It is recommended to type-specific set_* helpers instead of calling this directly.
     pub fn set_data(&mut self, data_type: Ip4Config2DataType, data: &mut [u8]) -> uefi::Result<()> {
         let status = unsafe {
             let data_ptr = data.as_mut_ptr() as *mut c_void;
@@ -45,6 +52,7 @@ impl Ip4Config2 {
         }
     }
 
+    /// Get configuration data.  It is recommended to type-specific get_* helpers instead of calling this directly.
     pub fn get_data(&mut self, data_type: Ip4Config2DataType) -> uefi::Result<Vec<u8>> {
         let mut data_size = 0;
 
@@ -69,11 +77,13 @@ impl Ip4Config2 {
         }
     }
 
+    /// Set config policy (static vs. dhcp).
     pub fn set_policy(&mut self, policy: Ip4Config2Policy) -> uefi::Result<()> {
         let mut data: [u8; 4] = policy.0.to_ne_bytes();
         self.set_data(Ip4Config2DataType::POLICY, &mut data)
     }
 
+    /// Get current interface configuration.
     pub fn get_interface_info(&mut self) -> uefi::Result<Ip4Config2InterfaceInfo> {
         let data = self.get_data(Ip4Config2DataType::INTERFACE_INFO)?;
         let info: &Ip4Config2InterfaceInfo =
@@ -100,6 +110,11 @@ impl Ip4Config2 {
         );
     }
 
+    /// Bring up network interface.  Does nothing in case the network
+    /// is already set up.  Otherwise turns on DHCP and waits until an
+    /// IPv4 address has been assigned.  Reports progress on the
+    /// console if verbose is set to true.  Returns TIMEOUT error in
+    /// case DHCP configuration does not finish within 30 seconds.
     pub fn ifup(&mut self, verbose: bool) -> uefi::Result<()> {
         let no_address = Ipv4Address::default();
 

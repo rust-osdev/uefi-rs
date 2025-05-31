f7965e9a4b9aca2cc8446515c0e8d2d6ea063418
Ren Trieu <ren.trieu.n@gmail.com>
2025-05-31 08:35:44 -0700

    uefi: Implementing wrappers for EFI Shell env and cur_dir functions
    This commit implements wrappers for the following EFI Shell Protocol
    functions: set_cur_dir() and get_cur_dir().

--- uefi/src/proto/shell/mod.rs
+++ uefi/src/proto/shell/mod.rs
@@ -2,12 +2,60 @@
 
 //! EFI Shell Protocol v2.2
 
-use crate::proto::unsafe_protocol;
+use uefi_macros::unsafe_protocol;
+use uefi_raw::Status;
+
+use core::ptr;
 
 pub use uefi_raw::protocol::shell::ShellProtocol;
+
+use crate::{CStr16, Char16};
 
 /// Shell Protocol
 #[derive(Debug)]
 #[repr(transparent)]
 #[unsafe_protocol(uefi_raw::protocol::shell::ShellProtocol::GUID)]
 pub struct Shell(uefi_raw::protocol::shell::ShellProtocol);
+
+impl Shell {
+    /// Returns the current directory on the specified device
+    ///
+    /// # Arguments
+    ///
+    /// * `file_system_mapping` - The file system mapping for which to get
+    ///   the current directory
+    /// # Returns
+    ///
+    /// * `Some(cwd)` - CStr16 containing the current working directory
+    /// * `None` - Could not retrieve current directory
+    #[must_use]
+    pub fn get_cur_dir<'a>(&'a self, file_system_mapping: Option<&CStr16>) -> Option<&'a CStr16> {
+        let mapping_ptr: *const Char16 = file_system_mapping.map_or(ptr::null(), |x| (x.as_ptr()));
+        let cur_dir = unsafe { (self.0.get_cur_dir)(mapping_ptr.cast()) };
+        if cur_dir.is_null() {
+            None
+        } else {
+            unsafe { Some(CStr16::from_ptr(cur_dir.cast())) }
+        }
+    }
+
+    /// Changes the current directory on the specified device
+    ///
+    /// # Arguments
+    ///
+    /// * `file_system` - Pointer to the file system's mapped name.
+    /// * `directory` - Points to the directory on the device specified by
+    ///   `file_system`.
+    /// # Returns
+    ///
+    /// * `Status::SUCCESS` The directory was successfully set
+    ///
+    /// # Errors
+    ///
+    /// * `Status::EFI_NOT_FOUND` The directory does not exist
+    pub fn set_cur_dir(&self, file_system: Option<&CStr16>, directory: Option<&CStr16>) -> Status {
+        let fs_ptr: *const Char16 = file_system.map_or(ptr::null(), |x| (x.as_ptr()));
+        let dir_ptr: *const Char16 = directory.map_or(ptr::null(), |x| (x.as_ptr()));
+        unsafe { (self.0.set_cur_dir)(fs_ptr.cast(), dir_ptr.cast()) }
+    }
+}

9be2728e63e57c67ac3d95dd30f96d82967c309f
Shank <shashanksharma21@gmail.com>
2026-09-04 10:05:11 -0400

    uefi-test-runner: add test for AbsolutePointerProtocol
    Adds an integration test for `AbsolutePointer`, in line with the existing
    `Pointer` test.

--- uefi-test-runner/src/proto/console/pointer.rs
+++ uefi-test-runner/src/proto/console/pointer.rs
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 use uefi::boot;
-use uefi::proto::console::pointer::Pointer;
+use uefi::proto::console::pointer::{AbsolutePointer, Pointer};
 
-pub fn test() {
+pub fn test_pointer() {
     info!("Running pointer protocol test");
     let handle = boot::get_handle_for_protocol::<Pointer>().expect("missing Pointer protocol");
     let mut pointer =
@@ -21,5 +21,30 @@ pub fn test() {
         info!("New pointer State: {state:#?}");
     } else {
         info!("Pointer state has not changed since the last query");
+    }
+}
+
+pub fn test_absolute_pointer() {
+    info!("Running absolute pointer protocol test");
+    let handle = boot::get_handle_for_protocol::<AbsolutePointer>()
+        .expect("missing AbsolutePointer protocol");
+    let mut pointer = boot::open_protocol_exclusive::<AbsolutePointer>(handle)
+        .expect("failed to open absolute pointer protocol");
+
+    pointer
+        .reset(false)
+        .expect("Failed to reset absolute pointer device");
+
+    let mode = pointer.mode();
+    info!("Absolute pointer mode: {mode:#?}");
+
+    let state = pointer
+        .read_state()
+        .expect("Failed to retrieve absolute pointer state");
+
+    if let Some(state) = state {
+        info!("New absolute pointer state: {state:#?}");
+    } else {
+        info!("Absolute pointer state has not changed since the last query");
     }
 }
